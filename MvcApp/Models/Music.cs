3b45924824a8ec38c497ce2bc9e55607098a24ae
Dvorachek <dylan.dvorachek@gmail.com>
2017-12-20 10:15:33 -0800

    Added music model/views/controller

---
+++ MvcApp/Models/Music.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcApp.Models
+{
+    public class Music
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public string Filename { get; set; }
+        public string Folder { get; set; }
+    }
+}
