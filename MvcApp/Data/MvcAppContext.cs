312d20226b3d053a264f35b6a32d166e7ca0f0a5
Dvorachek <dylan.dvorachek@gmail.com>
2017-12-20 10:45:50 -0800

    added db migrations

---
+++ MvcApp/Data/MvcAppContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcApp.Models
+{
+    public class MvcAppContext : DbContext
+    {
+        public MvcAppContext (DbContextOptions<MvcAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcApp.Models.Music> Music { get; set; }
+    }
+}
---
+++ MvcApp/Migrations/20171220182405_Initial.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace MvcApp.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Music",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Album = table.Column<string>(nullable: true),
+                    Artist = table.Column<string>(nullable: true),
+                    Filename = table.Column<string>(nullable: true),
+                    Folder = table.Column<string>(nullable: true),
+                    Genre = table.Column<string>(nullable: true),
+                    ReleaseDate = table.Column<DateTime>(nullable: false),
+                    Title = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Music", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Music");
+        }
+    }
+}
